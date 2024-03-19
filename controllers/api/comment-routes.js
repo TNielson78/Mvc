079d7396e21ea18746444e05c4778960e7050673
TNielson78 <travis.nielson01@gmail.com>
2024-03-18 18:17:43 -0600

    added comment box

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,7 +3,18 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', async (req, res) => {
 
+    const { title, body } = req.body;
+    const { user_id } = req.session;
+    const comment = await Comment.create({
+      id,
+      body,
+      user_id,
+    
+    });
+    res.json(comment);
+    });
 
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,6 +42,7 @@ router.get('/post/:id', async (req, res) => {
             ],
         });
         const post = postData.get({ plain: true });
+        console.log(post);
         res.render('post', {
             post,
             logged_in: req.session.logged_in,
